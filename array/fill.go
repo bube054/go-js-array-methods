4b0bea221df8189d8646d02828c772cd37d7f745
bube <attahgbube@gmail.com>
2024-02-10 23:12:31 +0100

    -

--- array/fill.go
+++ array/fill.go
@@ -2,34 +2,29 @@ package array
 
 import (
 	"errors"
-	"math"
 )
 
-// 1,2,3,4
 func Fill[T comparable](s []T, el T, startPos, endPos uint) ([]T, error) {
 	length := len(s)
 
-	newList := []T{}
-
 	if length == 0 {
-		return newList, nil
+		return nil, nil
 	}
 
-	if math.Abs(float64(startPos)) > float64(length-1) {
-		return []T{}, errors.New("start position is out of range")
+	if startPos < 0 || startPos >= uint(length) {
+		return nil, errors.New("start position is out of range")
 	}
 
-	if math.Abs(float64(endPos)) > float64(length-1) {
-		endPos = uint(length)
+	if endPos < startPos || endPos >= uint(length) {
+		return nil, errors.New("end position is out of range")
 	}
 
-	ForEach[T](s, func(e T, i int, s []T) {
-		if uint(i) >= startPos && uint(i) < endPos {
-			newList = append(newList, el)
-		} else {
-			newList = append(newList, e)
-		}
-	})
+	newList := make([]T, length)
+	copy(newList, s)
+
+	for i := startPos; i <= endPos; i++ {
+		newList[i] = el
+	}
 
 	return newList, nil
 }
