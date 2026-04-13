0a9d26b6f7c092defa11dcf2a81a811544aac923
vickbk <vickbake8@gmail.com>
2026-04-13 16:46:33 +0200

    refactor: improve type safety for Map Method but still lacks type inforcing due to lack of support of generic variables for methods

--- array/array.go
+++ array/array.go
@@ -100,7 +100,7 @@ func (a *Array[T]) LastIndexOf(element T, start *int) int {
 }
 
 // The Map() method creates a new array with the results of calling a function for every array element.
-func (a *Array[T]) Map(fn MapFunc[T]) []any {
+func (a *Array[T]) Map(fn MapFunc[T, any]) []any {
 	return Map([]T(*a), fn)
 }
 
