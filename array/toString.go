0fdc9ada2e2e3961acc0f3a97b14dbeb4251fd0c
Victoire Bake <58658800+vickbk@users.noreply.github.com>
2026-04-15 20:50:01 +0200

    fix: add type initializer for to string

--- array/toString.go
+++ array/toString.go
@@ -1,6 +1,6 @@
 package array
 
 // The ToString() function returns a string with slice values separated by commas. The ToString() function does not change the original slice.
-func ToString[T](slice []T) string {
+func ToString[T any](slice []T) string {
 	return Join(slice, nil)
 }
