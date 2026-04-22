a39a09fa418a60a869f3bd62bff5a6dc53945369
vickbk <vickbake8@gmail.com>
2026-04-22 12:33:48 +0200

    refactor: moving Array type definition to types file

--- array/array.go
+++ array/array.go
@@ -1,7 +1,5 @@
 package array
 
-type Array[T comparable] []T
-
 // The At() method returns an indexed element from the array and returns a possible error relating to out of range indexes.
 func (a Array[T]) At(index int) (T, error) {
 	return At([]T(a), index)
