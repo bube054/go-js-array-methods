d4cd34dfa983f157d1c3940d190177432005bbcf
vickbk <vickbake8@gmail.com>
2026-05-16 10:43:54 +0200

    refactor: optimize the Concat function performance

--- array/concat.go
+++ array/concat.go
@@ -3,21 +3,25 @@ package array
 // The concat() function concatenates (joins) two or more slices.
 // The concat() function returns a new slice, containing the joined slices.
 // The concat() function does not change the existing slices.
-func Concat[T comparable](slice1 []T, slice2 ...[]T) []T {
-	var all []T = slice1
-
-	for _, list := range slice2 {
-		all = append(all, list...)
+func Concat[S ~[]T, T any](slice1 S, slices ...S) S {
+	totalLen := len(slice1)
+	for _, s := range slices {
+		totalLen += len(s)
 	}
 
-	return all
+	result := make(S, 0, totalLen)
+
+	result = append(result, slice1...)
+
+	for _, s := range slices {
+		result = append(result, s...)
+	}
+
+	return result
 }
 
 // The Concat() method concatenates the array with other slices and returns a new concatenated array.
 func (a Array[T]) Concat(slices ...Array[T]) Array[T] {
-	b := make([][]T, len(slices))
-	for i, s := range slices {
-		b[i] = []T(s)
-	}
-	return Array[T](Concat([]T(a), b...))
+
+	return Concat(a, slices...)
 }
